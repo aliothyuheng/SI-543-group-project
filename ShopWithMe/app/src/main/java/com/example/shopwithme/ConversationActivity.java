ed8af806351ed52887a5c6ad86a9982fa4e2b0bc
nwmalzahn <nmalzahn@umich.edu>
2014-11-11 02:29:20 -0500

    Added CustomAdapter to Conversation Screen

--- ShopWithMe/app/src/main/java/com/example/shopwithme/ConversationActivity.java
+++ ShopWithMe/app/src/main/java/com/example/shopwithme/ConversationActivity.java
@@ -2,27 +2,51 @@ package com.example.shopwithme;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+
+import android.widget.ListView;
+
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ConversationActivity extends Activity {
 
     private EditText replyText;
+    private ListView replyListView;
+
+    private ArrayList<HashMap<String, Object>> replyList = new ArrayList<HashMap<String, Object>>();
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_conversation);
 		getActionBar().setTitle("Conversation");
+        replyListView = (ListView) findViewById(R.id.reply_list_view);
+        
+        initList();
 
+        CustomAdapter replyAdapter = new CustomAdapter(this, replyList, R.layout.reply_list_item,
+                new String[] {"name", "reply"}, new int[] {R.id.name, R.id.reply});
+        replyListView.setAdapter(replyAdapter);
+
+        /**
         Intent intent = getIntent();
         TextView tx = (TextView) findViewById(R.id.post);
         String postContent = intent.getStringExtra("post");
@@ -35,7 +59,9 @@ public class ConversationActivity extends Activity {
 	    registerForContextMenu(tv_3); **/
 	}
 
-/**
+
+
+    /**
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
@@ -88,6 +114,100 @@ public class ConversationActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+    static class ViewHolder {
+        //create a view holder class to hold all the views of one post
+        public TextView name;
+        public TextView reply;
+    }
+
+    private void initList() {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "Nick");
+        map.put("reply", "Yes! What time and where?");
+        replyList.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("name", "Lisa");
+        map.put("reply", "Can I tag along too?");
+        replyList.add(map);
+    }
+
+    public class CustomAdapter extends BaseAdapter {
+        private ArrayList<HashMap<String, Object>> mAppList;
+        private LayoutInflater mInflater;
+        private Context mContext;
+        private String[] keyString;
+        private int[] valueViewID;
+        private ViewHolder holder;
+
+        public CustomAdapter(Context c, ArrayList<HashMap<String, Object>> appList, int resource,
+                             String[] from, int[] to) {
+            mAppList = appList;
+            mContext = c;
+            mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            keyString = new String[from.length];
+            valueViewID = new int[to.length];
+            System.arraycopy(from, 0, keyString, 0, from.length);
+            System.arraycopy(to, 0, valueViewID, 0, to.length);
+        }
+
+        @Override
+        public int getCount() {
+            return mAppList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mAppList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            holder = null;
+
+            //set up the specific views for holder class
+            if (convertView != null) {
+                holder = (ViewHolder) convertView.getTag();
+            } else {
+                convertView = mInflater.inflate(R.layout.reply_list_item, null);
+                holder = new ViewHolder();
+                holder.name = (TextView) convertView.findViewById(valueViewID[0]);
+                holder.reply = (TextView) convertView.findViewById(valueViewID[1]);
+                convertView.setTag(holder);
+            }
+
+            //put information into holder
+            HashMap<String, Object> appInfo = mAppList.get(position);
+            if (appInfo != null) {
+                String posterName = (String) appInfo.get(keyString[0]);
+                String replyContent = (String) appInfo.get(keyString[1]);
+
+                holder.name.setText(posterName);
+                holder.reply.setText(replyContent);
+                //set up view profile for name text
+                viewProfile(holder.name);
+            }
+            return convertView;
+        }
+    }
+
+    public void viewProfile(final TextView tx) {
+        tx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ConversationActivity.this, Profile_display.class);
+                String name = tx.getText().toString();
+                //pass the user name to the profile display screen
+                intent.putExtra("UserName", name);
+                startActivity(intent);
+            }
+        });
+    }
 
     public void Reply(View view) {
         replyText = (EditText) findViewById(R.id.replyBox);
